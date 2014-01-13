32dcb3fcdb6f00f27d3db4f393892dbe41a0f544
Raymond <raymond.moore301@gmail.com>
2014-01-12 18:16:06 -0600

    removed Bootstrap 2 & replaced with Bootstrap 3. Fixing card creation page.

---
+++ api/controllers/CardController.js
@@ -0,0 +1,65 @@
+/**
+ * CardController
+ *
+ * @module      :: Controller
+ * @description	:: A set of functions called `actions`.
+ *
+ *                 Actions contain code telling Sails how to respond to a certain type of request.
+ *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
+ *
+ *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
+ *                 and/or override them with custom routes (`config/routes.js`)
+ *
+ *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
+ *
+ * @docs        :: http://sailsjs.org/#!documentation/controllers
+ */
+
+module.exports = {
+    
+  
+  /**
+   * Action blueprints:
+   *    `/card/new`
+   */
+   new: function (req, res) {
+      return res.view({ cardType: req.param('type') });
+  },
+
+
+  /**
+   * Action blueprints:
+   *    `/card/show`
+   */
+   show: function (req, res) {
+    
+    // Send a JSON response
+    return res.json({
+      hello: 'world'
+    });
+  },
+
+
+  /**
+   * Action blueprints:
+   *    `/card/delete`
+   */
+   delete: function (req, res) {
+    
+    // Send a JSON response
+    return res.json({
+      hello: 'world'
+    });
+  },
+
+
+
+
+  /**
+   * Overrides for the settings in `config/controllers.js`
+   * (specific to CardController)
+   */
+  _config: {}
+
+  
+};
---
+++ api/models/Card.js
@@ -0,0 +1,45 @@
+/**
+ * Card
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+    schema: true,
+
+    attributes: {
+        type: 'string',
+        title: {
+            type: 'string',
+            required: true
+        },
+        imagePath: {
+            type: 'string',
+            required: true
+        },
+        attack: 'integer',
+        defense: 'integer',
+        cost: 'integer',
+        quote: 'string',
+        specialAbilities: 'array',
+        createdBy: {
+            type: 'string',
+            required: true
+        },
+        createdAt: 'DATE',
+        private: {
+            type: 'boolean',
+            defaultsTo: false
+        }
+  },
+
+
+    // Lifecycle Callbacks
+    beforeCreate: function(values, next) {
+            values.createdAt = new Date;
+            next();
+    }
+
+};
